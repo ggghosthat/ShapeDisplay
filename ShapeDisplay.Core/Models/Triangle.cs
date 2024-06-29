8cecb49317e2c6bf3214ff3bc7b42ab4624b5133
ggghosthat <ildarildar990@gmail.com>
2024-06-29 22:44:34 +0300

    add triangle shape implementation

--- ShapeDisplay.Core/Models/Triangle.cs
+++ ShapeDisplay.Core/Models/Triangle.cs
@@ -1,6 +1,19 @@
-﻿namespace ShapeDisplay.Core.Models;
+﻿using System.Drawing;
+
+namespace ShapeDisplay.Core.Models;
 
 public class Triangle : Shape
 { 
+    public float Edge { get; set; }
+    public float Angle => 60f;
 
+    public override bool ContainsDot(Graphics graphics, PointF point)
+    {
+        return false;
+    }
+
+    public override Graphics PrintShape(Graphics graphics)
+    {
+        return graphics;
+    }
 }
